d057180d4c0661170ce6e003b55429638a7529c0
Cozzytree <hootowldhrubo@gmail.com>
2024-12-02 11:27:58 +0530

    updated types

--- src/_canvas/_components/canvasOptions.tsx
+++ src/_canvas/_components/canvasOptions.tsx
@@ -10,218 +10,224 @@ import { Square } from "lucide-react";
 import TextAlign from "./textalignOption";
 
 export default function CanvasOptions({
-  canvas,
-  activesShapes,
+   canvas,
+   activesShapes,
 }: {
-  activesShapes: number;
-  canvas: React.MutableRefObject<CanvasClass | null>;
+   activesShapes: number;
+   canvas: React.MutableRefObject<CanvasClass | null>;
 }) {
-  const handleColor = (color: string) => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+   const handleColor = (color: string) => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (
-          !canvas.current.canvasShapes[i] ||
-          !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
-        )
-          continue;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (
+               !canvas.current.canvasShapes[i] ||
+               !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
+            )
+               continue;
 
-        if (
-          canvas.current.canvasShapes[i].type === "line" ||
-          canvas.current.canvasShapes[i].type === "pencil"
-        ) {
-          canvas.current.canvasShapes[i].props.stroke = color;
-        } else if (canvas.current.canvasShapes[i].type === "text") {
-          canvas.current.canvasShapes[i].props.fontColor = color;
-        } else {
-          canvas.current.canvasShapes[i].props.fill = color;
-        }
+            if (
+               canvas.current.canvasShapes[i].type === "line" ||
+               canvas.current.canvasShapes[i].type === "pencil"
+            ) {
+               canvas.current.canvasShapes[i].props.stroke = color;
+            } else if (canvas.current.canvasShapes[i].type === "text") {
+               canvas.current.canvasShapes[i].props.fontColor = color;
+            } else {
+               canvas.current.canvasShapes[i].props.fill = color;
+            }
+         }
+
+         canvas.current.draw();
       }
+   };
 
-      canvas.current.draw();
-    }
-  };
+   const handleStroke = (stroke: number) => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-  const handleStroke = (stroke: number) => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (
+               !canvas.current.canvasShapes[i] ||
+               !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
+            )
+               continue;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (
-          !canvas.current.canvasShapes[i] ||
-          !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
-        )
-          continue;
+            canvas.current.canvasShapes[i].props.lineWidth = stroke;
+         }
 
-        canvas.current.canvasShapes[i].props.lineWidth = stroke;
+         canvas.current.draw();
       }
+   };
 
-      canvas.current.draw();
-    }
-  };
+   const handleStrokeColor = (color: string) => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-  const handleStrokeColor = (color: string) => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (
+               !canvas.current.canvasShapes[i] ||
+               !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
+            )
+               continue;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (
-          !canvas.current.canvasShapes[i] ||
-          !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
-        )
-          continue;
+            canvas.current.canvasShapes[i].props.stroke = color;
+         }
 
-        canvas.current.canvasShapes[i].props.stroke = color;
+         canvas.current.draw();
       }
+   };
 
-      canvas.current.draw();
-    }
-  };
+   const handleDashed = (v: [number, number]) => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-  const handleDashed = (v: [number, number]) => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (
+               !canvas.current.canvasShapes[i] ||
+               !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
+            )
+               continue;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (
-          !canvas.current.canvasShapes[i] ||
-          !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
-        )
-          continue;
+            canvas.current.canvasShapes[i].props.dash = v;
+         }
 
-        canvas.current.canvasShapes[i].props.dash = v;
+         canvas.current.draw();
       }
+   };
 
-      canvas.current.draw();
-    }
-  };
+   const handleFontSize = (v: number) => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-  const handleFontSize = (v: number) => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (
+               !canvas.current.canvasShapes[i] ||
+               !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
+            )
+               continue;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (
-          !canvas.current.canvasShapes[i] ||
-          !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
-        )
-          continue;
+            canvas.current.canvasShapes[i].props.fontSize = v;
+         }
 
-        canvas.current.canvasShapes[i].props.fontSize = v;
+         canvas.current.draw();
       }
+   };
 
-      canvas.current.draw();
-    }
-  };
+   const handleRadius = (v: boolean) => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-  const handleRadius = (v: boolean) => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (
+               !canvas.current.canvasShapes[i] ||
+               !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
+            )
+               continue;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (
-          !canvas.current.canvasShapes[i] ||
-          !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
-        )
-          continue;
+            canvas.current.canvasShapes[i].props.radius = v ? 8 : 0;
+         }
 
-        canvas.current.canvasShapes[i].props.radius = v ? 8 : 0;
+         canvas.current.draw();
       }
+   };
 
-      canvas.current.draw();
-    }
-  };
+   const handleAlign = (side: "left" | "center" | "right") => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-  const handleAlign = (side: "left" | "center" | "right") => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (
+               !canvas.current.canvasShapes[i] ||
+               !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
+            )
+               continue;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (
-          !canvas.current.canvasShapes[i] ||
-          !cConf.activeShapes.has(canvas.current.canvasShapes[i].id)
-        )
-          continue;
+            canvas.current.canvasShapes[i].props.textAlign = side;
+         }
 
-        canvas.current.canvasShapes[i].props.textAlign = side;
+         canvas.current.draw();
       }
+   };
 
-      canvas.current.draw();
-    }
-  };
+   const handleIndex = (where: "above" | "bottom") => {
+      if (!canvas.current || !canvas.current.canvasShapes) return;
 
-  const handleIndex = (where: "above" | "bottom") => {
-    if (!canvas.current || !canvas.current.canvasShapes) return;
+      let index: number | null = null;
 
-    let index: number | null = null;
+      if (Array.isArray(canvas.current.canvasShapes)) {
+         for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
+            if (!canvas.current.canvasShapes[i]) continue;
 
-    if (Array.isArray(canvas.current.canvasShapes)) {
-      for (let i = 0; i < canvas.current.canvasShapes.length; i++) {
-        if (!canvas.current.canvasShapes[i]) continue;
-
-        if (cConf.activeShapes.has(canvas.current.canvasShapes[i].id)) {
-          index = i;
-          break;
-        }
+            if (cConf.activeShapes.has(canvas.current.canvasShapes[i].id)) {
+               index = i;
+               break;
+            }
+         }
+         if (index != null) {
+            if (where === "above") {
+               [
+                  canvas.current.canvasShapes[index],
+                  canvas.current.canvasShapes[
+                     canvas.current.canvasShapes.length - 1
+                  ],
+               ] = [
+                  canvas.current.canvasShapes[
+                     canvas.current.canvasShapes.length - 1
+                  ],
+                  canvas.current.canvasShapes[index],
+               ];
+            } else {
+               [
+                  canvas.current.canvasShapes[index],
+                  canvas.current.canvasShapes[0],
+               ] = [
+                  canvas.current.canvasShapes[0],
+                  canvas.current.canvasShapes[index],
+               ];
+            }
+            canvas.current.draw();
+         }
       }
-      if (index != null) {
-        if (where === "above") {
-          [
-            canvas.current.canvasShapes[index],
-            canvas.current.canvasShapes[canvas.current.canvasShapes.length - 1],
-          ] = [
-            canvas.current.canvasShapes[canvas.current.canvasShapes.length - 1],
-            canvas.current.canvasShapes[index],
-          ];
-        } else {
-          [canvas.current.canvasShapes[index], canvas.current.canvasShapes[0]] =
-            [
-              canvas.current.canvasShapes[0],
-              canvas.current.canvasShapes[index],
-            ];
-        }
-        canvas.current.draw();
-      }
-    }
-  };
+   };
 
-  return (
-    <div className="absolute z-[100] bottom-20 w-full flex flex-col items-center justify-center">
-      <Menubar>
-        <FillOption handleColor={handleColor} />
-        <Stroke_Option
-          handleStroke={handleStroke}
-          handleStrokeColor={handleStrokeColor}
-        />
-        {/* {activesShapes <= 1 && <DashedOption handleDashed={handleDashed} />} */}
-        <DashedOption handleDashed={handleDashed} />
-        <FontSizeoption handleFontSize={handleFontSize} />
-        <RadiusOption handleRadius={handleRadius} />
-        {/* <Forward /> */}
-        {activesShapes > 0 && (
-          <>
-            <div
-              role="button"
-              onClick={() => handleIndex("bottom")}
-              className="flex relative px-2"
-            >
-              <Square />
-              <Square className="absolute top-[0.3em] left-3 bg-background" />
-            </div>
-            <div
-              role="button"
-              onClick={() => handleIndex("above")}
-              className="flex relative px-2"
-            >
-              <Square className="z-20 bg-background" />
-              <Square className="absolute top-[0.3em] left-[0.3em]" />
-            </div>
-          </>
-        )}
-      </Menubar>
+   return (
+      <div className="absolute z-[100] bottom-20 w-full flex flex-col items-center justify-center">
+         <Menubar>
+            <FillOption handleColor={handleColor} />
+            <Stroke_Option
+               handleStroke={handleStroke}
+               handleStrokeColor={handleStrokeColor}
+            />
+            {/* {activesShapes <= 1 && <DashedOption handleDashed={handleDashed} />} */}
+            <DashedOption handleDashed={handleDashed} />
+            <FontSizeoption handleFontSize={handleFontSize} />
+            <RadiusOption handleRadius={handleRadius} />
+            {/* <Forward /> */}
+            {activesShapes > 0 && (
+               <>
+                  <div
+                     role="button"
+                     onClick={() => handleIndex("bottom")}
+                     className="flex relative px-2"
+                  >
+                     <Square />
+                     <Square className="absolute top-[0.3em] left-3 bg-background" />
+                  </div>
+                  <div
+                     role="button"
+                     onClick={() => handleIndex("above")}
+                     className="flex relative px-2"
+                  >
+                     <Square className="z-20 bg-background" />
+                     <Square className="absolute top-[0.3em] left-[0.3em]" />
+                  </div>
+               </>
+            )}
+         </Menubar>
 
-      {activesShapes === 1 && <TextAlign handleAlign={handleAlign} />}
-    </div>
-  );
+         {activesShapes === 1 && <TextAlign handleAlign={handleAlign} />}
+      </div>
+   );
 }
